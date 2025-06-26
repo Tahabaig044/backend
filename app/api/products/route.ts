26aad06793611f2b41e0bc9e7657869bb9ab16f2
Tahabaig044 <135549410+Tahabaig044@users.noreply.github.com>
2025-06-26 18:11:49 +0500

    Update route.ts

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -7,11 +7,11 @@ import Collection from "@/lib/models/Collection";
 
 export const POST = async (req: NextRequest) => {
   try {
-    const { userId } = auth();
+    // const { userId } = auth();
 
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
+    // if (!userId) {
+    //   return new NextResponse("Unauthorized", { status: 401 });
+    // }
 
     await connectToDB();
 
