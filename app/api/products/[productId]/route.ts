e7785261f4ffb786a8bcb07d6c6dddc30cf23e4a
Tahabaig044 <135549410+Tahabaig044@users.noreply.github.com>
2025-06-26 18:15:11 +0500

    Update route.ts

--- app/api/products/[productId]/route.ts
+++ app/api/products/[productId]/route.ts
@@ -137,11 +137,11 @@ export const DELETE = async (
   { params }: { params: { productId: string } }
 ) => {
   try {
-    const { userId } = auth();
+    // const { userId } = auth();
 
-    if (!userId) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
+    // if (!userId) {
+    //   return new NextResponse("Unauthorized", { status: 401 });
+    // }
 
     await connectToDB();
 
